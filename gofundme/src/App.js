384c9af2d6ac46b7c15a5325e9dffbc55818cde0
rafidhossain1995 <rafidhossain@pursuit.org>
2020-02-19 19:22:43 -0500

    create topbar for gofundme

---
+++ gofundme/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TopBar from "./Component/topbar"
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ gofundme/src/Component/topbar.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class TopBar extends Component{
+    
+    render(){
+        return(
+         <div className = "topBar">
+              Go Fund Me
+         <br/>
+             Help me go on a vacation to a beach somewhere
+        </div>
+        )
+    }
+      
+}
+
+export default TopBar
